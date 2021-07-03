450b3b4415a90c953c290bdcfe244fdeb2e1a4ee
dong50252409 <304887750@qq.com>
2021-07-03 16:28:20 +0800

    jps寻路

--- src/jps.erl
+++ src/jps.erl
@@ -6,20 +6,13 @@
 
 
 search(StartGrid, EndGrid, ValidFun) ->
-    Score = f(StartGrid, EndGrid),
-    OpenGrids = add_grid(Score, StartGrid, [], gb_trees:empty()),
+    OpenGrids = add_grid(0, StartGrid, {0, []}, gb_trees:empty()),
     VisitedGrids = #{},
     case do_search(EndGrid, ValidFun, OpenGrids, VisitedGrids) of
         none ->
             none;
         Path ->
             Path
-%%            case proplists:get_value(trace_path, Options, false) of
-%%                true ->
-%%                    ok;
-%%                false ->
-%%                    trace_path(StartGrid, Path)
-%%            end
     end.
 
 do_search(EndGrid, ValidFun, OpenGrids, VisitedGrids) ->
@@ -28,12 +21,12 @@ do_search(EndGrid, ValidFun, OpenGrids, VisitedGrids) ->
             none;
         false ->
             case gb_trees:take_smallest(OpenGrids) of
-                {{_, EndGrid}, Path, _OpenGrids1} ->
+                {{_, EndGrid}, {_G, Path}, _OpenGrids1} ->
                     [EndGrid | Path];
-                {{_Score, Grid}, Path, OpenGrids1} ->
+                {{_Score, Grid}, {G, Path}, OpenGrids1} ->
                     Directions = direction(Grid, Path),
                     NeighbourGrids = search_neighbour_girds(EndGrid, ValidFun, Grid, VisitedGrids, Directions, []),
-                    {OpenGrids2, VisitedGrids1} = add_neighbour_girds(EndGrid, NeighbourGrids, [Grid | Path], OpenGrids1, VisitedGrids),
+                    {OpenGrids2, VisitedGrids1} = add_neighbour_girds(EndGrid, NeighbourGrids, G, [Grid | Path], OpenGrids1, VisitedGrids),
                     draw_map(OpenGrids2),
                     do_search(EndGrid, ValidFun, OpenGrids2, VisitedGrids1)
             end
@@ -130,16 +123,14 @@ check_neighbour_grid({X, Y}, ValidFun, {XOffset, YOffset}) ->
     (not ValidFun({X, Y1}) andalso ValidFun({X + XOffset, Y1}))
         orelse (not ValidFun({X1, Y}) andalso ValidFun({X1, Y + YOffset})).
 
-add_neighbour_girds(EndGrid, [Grid | T], Path, OpenGrids, VisitedGrids) ->
-    Score = f(Grid, EndGrid),
-    OpenGrids1 = add_grid(Score, Grid, Path, OpenGrids),
+add_neighbour_girds(EndGrid, [Grid | T], G, Path, OpenGrids, VisitedGrids) ->
+    G1 = G + g(Grid, EndGrid),
+    Score = G1 + h(Grid, EndGrid),
+    OpenGrids1 = add_grid(Score, Grid, {G1, Path}, OpenGrids),
     VisitedGrids1 = VisitedGrids#{Grid => true},
-    add_neighbour_girds(EndGrid, T, Path, OpenGrids1, VisitedGrids1);
-add_neighbour_girds(_EndGrid, [], _Path, OpenGrids, VisitedGrids) ->
+    add_neighbour_girds(EndGrid, T, G, Path, OpenGrids1, VisitedGrids1);
+add_neighbour_girds(_EndGrid, [], _G, _Path, OpenGrids, VisitedGrids) ->
     {OpenGrids, VisitedGrids}.
-
-f(Grid1, Grid2) ->
-    g(Grid1, Grid2) + h(Grid1, Grid2).
 
 g({X1, Y1}, {X2, Y2}) ->
     X3 = erlang:abs(X1 - X2),
@@ -165,14 +156,14 @@ add_grid(Score, Grid, Path, OpenGrids) ->
 %%============================================================
 test() ->
 %%    Map = {{32, 32, 32, 32, 32, 88, 32, 32, 32, 32}, {32, 32, 32, 32, 32, 32, 32, 88, 32, 32}, {32, 32, 88, 32, 32, 32, 32, 32, 32, 32}, {32, 32, 32, 32, 88, 32, 32, 32, 32, 32}, {32, 32, 32, 32, 32, 32, 32, 32, 32, 32}, {32, 32, 32, 32, 32, 32, 88, 88, 32, 32}, {32, 32, 32, 32, 32, 32, 32, 32, 32, 32}, {32, 32, 32, 32, 32, 32, 32, 32, 32, 32}, {32, 32, 32, 32, 32, 32, 88, 32, 32, 32}, {32, 32, 88, 88, 32, 32, 88, 32, 32, 32}},
-    Map = {{32,32,32,32,32,32,32,32,32,32},{88,32,32,32,32,32,32,32,32,32},{32,32,32,32,32,32,88,32,32,88},{32,88,32,32,32,32,88,32,32,32},{32,32,88,32,32,32,32,32,88,32},{32,32,32,88,32,32,32,32,32,32},{32,32,32,32,32,32,32,32,32,32},{32,32,32,32,32,32,32,32,32,32},{32,88,32,32,32,32,32,32,32,88},{32,32,32,32,32,32,32,32,32,32}},
+    Map = {{32, 32, 32, 32, 32, 32, 32, 32, 32, 32}, {88, 32, 32, 32, 32, 32, 32, 32, 32, 32}, {32, 32, 32, 32, 32, 32, 88, 32, 32, 88}, {32, 88, 32, 32, 32, 32, 88, 32, 32, 32}, {32, 32, 88, 32, 32, 32, 32, 32, 88, 32}, {32, 32, 32, 88, 32, 32, 32, 32, 32, 32}, {32, 32, 32, 32, 32, 32, 32, 32, 32, 32}, {32, 32, 32, 32, 32, 32, 32, 32, 32, 32}, {32, 88, 32, 32, 32, 32, 32, 32, 32, 88}, {32, 32, 32, 32, 32, 32, 32, 32, 32, 32}},
     put(map, Map),
     ValidFun =
         fun({X, Y}) ->
             X =< 10 andalso Y =< 10 andalso 0 < X andalso 0 < Y
                 andalso element(X, element(Y, Map)) =:= 32
         end,
-    search({1, 1}, {10, 10}, ValidFun).
+    search({1, 1}, {1, 10}, ValidFun).
 
 take(OpenGrids, Map) ->
     case gb_trees:is_empty(OpenGrids) of
