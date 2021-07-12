2caa3489481ab7acf91e37f140bc05bfe97fdb45
dong50252409 <304887750@qq.com>
2021-07-12 11:10:36 +0800

    优化jps寻路性能

--- src/jps.erl
+++ src/jps.erl
@@ -23,15 +23,12 @@
 
 -spec search(StartGrid :: grid(), EndGrid :: grid(), ValidFun :: fun((grid()) -> boolean()), Options :: options()) -> result().
 search(StartGrid, EndGrid, ValidFun, Options) ->
-    OpenGrids = gb_trees:empty(),
-    VisitedGrids = #{},
-    Directions = [{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}],
-    JumpGrids = get_jump_grids(EndGrid, ValidFun, StartGrid, VisitedGrids, Directions),
-    {OpenGrids1, VisitedGrids1} = add_jump_grids(EndGrid, StartGrid, 0, [StartGrid], OpenGrids, VisitedGrids, JumpGrids),
-%%    draw_map(OpenGrids1),
+    G = 0,
+    Score = G + h(StartGrid, EndGrid),
+    OpenGrids = gb_trees:insert({Score, StartGrid}, {G, []}, gb_trees:empty()),
+    VisitedGrids = #{StartGrid => G},
     MaxLimit = proplists:get_value(max_limit, Options, ?DEFAULT_MAX_LIMIT),
-    do_search(EndGrid, ValidFun, OpenGrids1, VisitedGrids1, MaxLimit).
-
+    do_search(EndGrid, ValidFun, OpenGrids, VisitedGrids, MaxLimit).
 
 -spec get_full_path(JumpPoints :: [grid()]) -> {full_path, Path :: [grid()]}.
 get_full_path(JumpPoints) ->
@@ -42,37 +39,31 @@ get_full_path(JumpPoints) ->
 %%% Internal Functions
 %%%======================================================================
 do_search(EndGrid, ValidFun, OpenGrids, VisitedGrids, MaxLimit) when MaxLimit > 0 ->
-    case take_grid(OpenGrids, VisitedGrids) of
-        none ->
-            none;
-        {EndGrid, _G, Path, _OpenGrids1, _VisitedGrids1} ->
-            {jump_points, lists:reverse([EndGrid | Path])};
-        {Grid, G, Path, OpenGrids1, VisitedGrids1} ->
-            ParentGrid = hd(Path),
-            Directions = get_directions(ValidFun, Grid, ParentGrid),
-%%            io:format("Directions:~w~n", [Directions]),
-%%            io:format("take_grid Grid:~w ParentGrid:~w G:~w~n", [Grid, ParentGrid, G]),
-            JumpGrids = get_jump_grids(EndGrid, ValidFun, Grid, VisitedGrids1, Directions),
-            {OpenGrids2, VisitedGrids2} = add_jump_grids(EndGrid, Grid, G, [Grid | Path], OpenGrids1, VisitedGrids1, JumpGrids),
-%%            draw_map(OpenGrids2),
-            do_search(EndGrid, ValidFun, OpenGrids2, VisitedGrids2, MaxLimit - 1)
-    end;
-do_search(_EndGrid, _ValidFun, _OpenGrids, _VisitedGrids, _MaxLimit) ->
-    max_limited.
-
-take_grid(OpenGrids, VisitedGrids) ->
+%%    draw_map(OpenGrids),
     case gb_trees:is_empty(OpenGrids) of
         true ->
             none;
         false ->
-            {{_Score, Grid}, {G, Path}, OpenGrids1} = gb_trees:take_smallest(OpenGrids),
-            VisitedGrids1 = VisitedGrids#{Grid := -1},
-            {Grid, G, Path, OpenGrids1, VisitedGrids1}
-    end.
+            case gb_trees:take_smallest(OpenGrids) of
+                {{_Score, EndGrid}, {_G, Path}, _OpenGrids1} ->
+                    {jump_points, lists:reverse([EndGrid | Path])};
+                {{_Score, Grid}, {G, Path}, OpenGrids1} ->
+                    Directions = get_directions(ValidFun, Grid, Path),
+%%                    io:format("take_grid Grid:~w ParentGrid:~w G:~w~n Directions:~w~n", [Grid, ParentGrid, G, Directions]),
+                    VisitedGrids1 = VisitedGrids#{Grid := -1},
+                    JumpGrids = get_jump_grids(EndGrid, ValidFun, Grid, VisitedGrids1, Directions),
+                    {OpenGrids2, VisitedGrids2} = add_jump_grids(EndGrid, Grid, G, [Grid | Path], OpenGrids1, VisitedGrids1, JumpGrids),
+                    do_search(EndGrid, ValidFun, OpenGrids2, VisitedGrids2, MaxLimit - 1)
+            end
+    end;
+do_search(_EndGrid, _ValidFun, _OpenGrids, _VisitedGrids, _MaxLimit) ->
+    max_limited.
 
-get_directions(ValidFun, Grid, ParentGrid) ->
+get_directions(ValidFun, Grid, [ParentGrid | _]) ->
     {DX, DY} = get_direction(Grid, ParentGrid),
-    directions_1(ValidFun, Grid, DX, DY).
+    directions_1(ValidFun, Grid, DX, DY);
+get_directions(_ValidFun, _Grid, []) ->
+    [{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}].
 
 directions_1(ValidFun, {X, Y}, DX, 0) ->
     NeighbourGirds = [{DX, 0}],
@@ -126,7 +117,6 @@ get_jump_gird(EndGrid, ValidFun, VisitedGrids, {X, Y}, DX, DY) ->
             end
     end.
 
-
 get_jump_grid_1(EndGrid, ValidFun, VisitedGrids, NeighbourGrid, DX, DY) when DX =:= 0; DY =:= 0 ->
     case check_jump_grid(ValidFun, NeighbourGrid, DX, DY) of
         true ->
@@ -162,14 +152,15 @@ add_jump_grids(EndGrid, ParentGrid, G, Path, OpenGrids, VisitedGrids, [Grid | T]
             OpenGrids2 = OpenGrids,
             VisitedGrids1 = VisitedGrids;
         #{Grid := OldScore} when OldScore > Score ->
+%%            io:format("replace_add_grids OldScore:~w Score:~w Grid:~w G1:~w~n", [OldScore, Score, Grid, G1]),
             OpenGrids1 = gb_trees:delete({OldScore, Grid}, OpenGrids),
             OpenGrids2 = gb_trees:insert({Score, Grid}, {G1, Path}, OpenGrids1),
             VisitedGrids1 = VisitedGrids#{Grid => Score};
         _ ->
+%%            io:format("add_grids Score:~w Grid:~w G1:~w~n", [Score, Grid, G1]),
             OpenGrids2 = gb_trees:insert({Score, Grid}, {G1, Path}, OpenGrids),
             VisitedGrids1 = VisitedGrids#{Grid => Score}
     end,
-%%    io:format("add_grids Score:~w,Grid:~w,G1:~w~n", [Score, Grid, G1]),
     add_jump_grids(EndGrid, ParentGrid, G, Path, OpenGrids2, VisitedGrids1, T);
 add_jump_grids(_EndGrid, _ParentGrid, _G, _Path, OpenGrids, VisitedGrids, []) ->
     {OpenGrids, VisitedGrids}.
@@ -177,12 +168,7 @@ add_jump_grids(_EndGrid, _ParentGrid, _G, _Path, OpenGrids, VisitedGrids, []) ->
 g({X1, Y1}, {X2, Y2}) ->
     X3 = erlang:abs(X1 - X2),
     Y3 = erlang:abs(Y1 - Y2),
-    case X3 > Y3 of
-        true ->
-            14 * Y3 + 10 * (X3 - Y3);
-        false ->
-            14 * X3 + 10 * (Y3 - X3)
-    end.
+    ?IF(X3 > Y3, 14 * Y3 + 10 * (X3 - Y3), 14 * X3 + 10 * (Y3 - X3)).
 
 h({X1, Y1}, {X2, Y2}) ->
     (erlang:abs(X1 - X2) + erlang:abs(Y1 - Y2)) * 10.
@@ -206,28 +192,77 @@ get_full_path_2({X, Y}, Grid2, DX, DY, Path) ->
 %%============================================================
 -ifdef(TEST).
 -compile(export_all).
-test(Start, End, Map) ->
-    put(map, Map),
-    ValidFun =
+
+test(Row, Col, BlockNum, Num, IsShow) ->
+    WorldMaps = [gen_map(Row, Col, BlockNum) || _ <- lists:seq(1, Num)],
+    file:write_file("world_maps.data", term_to_binary(WorldMaps)),
+    test_1(Row, Col, IsShow, WorldMaps).
+
+test(Row, Col, IsShow) ->
+    {ok, Bin} = file:read_file("world_maps.data"),
+    WorldMaps = binary_to_term(Bin),
+    test_1(Row, Col, IsShow, WorldMaps).
+
+test_1(Row, Col, IsShow, WorldMaps) ->
+    {ok, IO} = file:open("world_maps.out", [write]),
+    put(io, IO),
+    loop_world_map({1, 1}, {Row, Col}, Row, Col, WorldMaps, IsShow),
+    file:close(IO).
+
+loop_world_map(Start, End, Row, Col, [WorldMap | T], IsShow) ->
+    put(world_map, WorldMap),
+    Fun =
         fun({X, Y}) ->
-            X =< 10 andalso Y =< 10 andalso 0 < X andalso 0 < Y
-                andalso element(X, element(Y, Map)) =/= $X
+            X > 0 andalso X =< Row andalso Y > 0 andalso Y =< Col
+                andalso element(X, element(Y, WorldMap)) =/= $X
         end,
-    search(Start, End, ValidFun, []).
+    case jps:search(Start, End, Fun, []) of
+        {jump_points, Path} ->
+            {full_path, FullPath} = jps:get_full_path(Path);
+        _Other ->
+            FullPath = []
+    end,
+    ?IF(IsShow, draw_map(Row, FullPath, WorldMap), ok),
+    loop_world_map(Start, End, Row, Col, T, IsShow);
+loop_world_map(_Start, _End, _Row, _Col, [], _IsShow) ->
+    ok.
 
-take(OpenGrids, Map) ->
-    case gb_trees:is_empty(OpenGrids) of
-        true ->
-            Map;
-        false ->
-            {{_Score, {X, Y}}, _Parent, OpenGrids1} = gb_trees:take_smallest(OpenGrids),
-            take(OpenGrids1, setelement(Y, Map, setelement(X, element(Y, Map), $P)))
-    end.
+gen_map(Row, Col, BlockNum) ->
+    WorldMap = list_to_tuple(lists:duplicate(Col, list_to_tuple(lists:duplicate(Row, $ )))),
+    gen_block(Row, Col, BlockNum, WorldMap).
 
+gen_block(Row, Col, BlockNum, WorldMap) when BlockNum > 0 ->
+    case {rand:uniform(Row), rand:uniform(Col)} of
+        XY when XY =:= {1, 1}; XY =:= {Row, 1}; XY =:= {1, Col}; XY =:= {Row, Col} ->
+            gen_block(Row, Col, BlockNum, WorldMap);
+        {X, Y} ->
+            C = element(Y, WorldMap),
+            case element(X, C) =/= $X of
+                true ->
+                    WorldMap1 = setelement(Y, WorldMap, setelement(X, C, $X)),
+                    gen_block(Row, Col, BlockNum - 1, WorldMap1);
+                false ->
+                    gen_block(Row, Col, BlockNum, WorldMap)
+            end
+    end;
+gen_block(_Row, _Col, _BlockNum, WorldMap) ->
+    WorldMap.
+
+draw_map(Width, Path, WorldMap) ->
+    WorldMap1 = draw_map_path(Path, WorldMap),
+    Border = io_lib:format("~s~n", [lists:duplicate(Width + 2, $X)]),
+    Str = [io_lib:format("X~sX~n", [tuple_to_list(Row)]) || Row <- tuple_to_list(WorldMap1)],
+    io:format(get(io), "~s~n", [[Border, Str, Border]]).
+
+draw_map_path([{X, Y} | T], WorldMap) ->
+    WorldMap1 = setelement(Y, WorldMap, setelement(X, element(Y, WorldMap), $o)),
+    draw_map_path(T, WorldMap1);
+draw_map_path(_, WorldMap) ->
+    WorldMap.
 
 draw_map(OpenGrids) ->
-    Map1 = take(OpenGrids, get(map)),
-    put(map, Map1),
+    Map1 = take(OpenGrids, get(world_map)),
+    put(world_map, Map1),
     io:format("   12345678910~n"),
     io:format("  XXXXXXXXXXXX~n"),
     Fun =
@@ -237,4 +272,13 @@ draw_map(OpenGrids) ->
         end,
     _ = lists:foldl(Fun, 1, tuple_to_list(Map1)),
     io:format("  XXXXXXXXXXXX~n").
+
+take(OpenGrids, Map) ->
+    case gb_trees:is_empty(OpenGrids) of
+        true ->
+            Map;
+        false ->
+            {{_Score, {X, Y}}, _Parent, OpenGrids1} = gb_trees:take_smallest(OpenGrids),
+            take(OpenGrids1, setelement(Y, Map, setelement(X, element(Y, Map), $P)))
+    end.
 -endif.
